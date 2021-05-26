5ed569aa0d4777a815259edefba288a543826284
Lucas Luz <LUCAS_LUZ@jabil.com>
2021-05-26 10:22:51 -0300

    Verifying the start and the end of pdf

--- src/components/pdf/index.jsx
+++ src/components/pdf/index.jsx
@@ -1,27 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Document, Page } from "react-pdf/dist/umd/entry.webpack";
 
 export default function Pdf({ fileName }) {
 	const [numPages, setNumPages] = useState(null);
 	const [pageNumber, setPageNumber] = useState(1);
+	const previousButton = useRef(null);
+	const nextButton = useRef(null);
 
 	const onDocumentLoadSuccess = ({ numPages }) => {
 		setNumPages(numPages);
 	};
 
 	const previousPage = () => {
-		if (pageNumber > 1) setPageNumber(pageNumber - 1);
+		if (pageNumber > 1) {
+			setPageNumber(pageNumber - 1);
+		}
 	};
 
 	const nextPage = () => {
-		if (pageNumber < numPages) setPageNumber(pageNumber + 1);
+		if (pageNumber < numPages) {
+			setPageNumber(pageNumber + 1);
+		}
 	};
+
+	useEffect(() => {
+		if (previousButton) {
+			if (pageNumber === 1) previousButton.current.disabled = true;
+			else previousButton.current.disabled = false;
+		}
+
+		if (nextButton) {
+			if (pageNumber === numPages) nextButton.current.disabled = true;
+			else nextButton.current.disabled = false;
+		}
+	}, [pageNumber, numPages]);
 
 	return (
 		<>
 			<nav>
-				<button onClick={previousPage}>Previous</button>
-				<button onClick={nextPage}>Next</button>
+				<button ref={previousButton} onClick={previousPage}>
+					Previous
+				</button>
+				<button ref={nextButton} onClick={nextPage}>
+					Next
+				</button>
 			</nav>
 			<Document file={fileName} onLoadSuccess={onDocumentLoadSuccess}>
 				<Page pageNumber={pageNumber} />
